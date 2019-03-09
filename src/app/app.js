0c63b824427d954853d8333dad34c85dbbb4d20e
Marcin Groszkiewicz <m.groszkiewicz@bioinfo.pl>
2019-03-09 19:58:04 +0100

    prepare basic app structure

---
+++ src/app/app.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <header id="app-header">SUNNY OR NOT</header>
+        <main>
+          <section id="main-day">...</section>
+          <section id="next-days">...</section>
+        </main>
+        <footer id="app-footer">PLACEHOLDER</footer>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
