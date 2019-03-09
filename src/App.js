fc0410252a87c430d1f271c6d7aaa87dd1690584
Marcin Groszkiewicz <m.groszkiewicz@bioinfo.pl>
2019-03-09 13:33:42 +0100

    test prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <dic>With dev</dic>
-    );
+    return <div>With dev</div>;
   }
 }
 
